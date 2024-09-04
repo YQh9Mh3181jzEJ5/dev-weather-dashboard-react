e3e032d8b55e92fb21f07fc69527e48780e52b3b
X58okYW8B54e <P4v8AU7pe87R@users.noreply.github.com>
2024-09-05 07:31:48 +0900

    add: .gitignore

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,11 +1,34 @@
 import { FaSearch } from "react-icons/fa";
-import { WiDaySunny, WiStrongWind, WiHumidity } from "react-icons/wi";
+import {
+  WiDaySunny,
+  WiStrongWind,
+  WiHumidity,
+  WiDayCloudy,
+  WiNightAltCloudy,
+  WiCloud,
+  WiCloudy,
+  WiShowers,
+  WiDayRain,
+  WiNightAltRain,
+  WiThunderstorm,
+  WiSnow,
+  WiFog,
+} from "react-icons/wi";
 import { IconType } from "react-icons";
+import { useEffect, useState } from "react";
 interface WeatherInfoProps {
   icon: IconType;
   value: string;
   label: string;
   unit: string;
+}
+
+interface WeatherData {
+  icon: string;
+  temperature: number;
+  humidity: number;
+  windSpeed: number;
+  location: string;
 }
 
 const WeatherInfo = ({ icon: Icon, value, label, unit }: WeatherInfoProps) => (
@@ -22,6 +45,55 @@ const WeatherInfo = ({ icon: Icon, value, label, unit }: WeatherInfoProps) => (
 );
 
 const Weather = () => {
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const allIcons: { [key: string]: IconType } = {
+    "01d": WiDaySunny,
+    "01n": WiDaySunny,
+    "02d": WiDayCloudy,
+    "02n": WiNightAltCloudy,
+    "03d": WiCloud,
+    "03n": WiCloud,
+    "04d": WiCloudy,
+    "04n": WiCloudy,
+    "09d": WiShowers,
+    "09n": WiShowers,
+    "10d": WiDayRain,
+    "10n": WiNightAltRain,
+    "11d": WiThunderstorm,
+    "11n": WiThunderstorm,
+    "13d": WiSnow,
+    "13n": WiSnow,
+    "50d": WiFog,
+    "50n": WiFog,
+  };
+  const search = async (city: string) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setWeatherData({
+        icon: data.weather[0].icon,
+        temperature: Math.floor(data.main.temp),
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        location: data.name,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    search("tokyo");
+  }, []);
+
+  const WeatherIcon = ({ iconCode }: { iconCode: string }) => {
+    const Icon = allIcons[iconCode] || WiDaySunny;
+    return <Icon className="w-40 h-40 text-slate-200" />;
+  };
+
   return (
     <div className="w-full min-h-screen flex flex-col items-center justify-center bg-red-800">
       <h1 className="text-4xl font-bold mb-4 text-slate-200">Weather Today</h1>
@@ -40,25 +112,33 @@ const Weather = () => {
             <FaSearch />
           </div>
         </div>
-        <WiDaySunny className="w-40 h-40 text-slate-200" />
+        {weatherData && <WeatherIcon iconCode={weatherData.icon} />}
         <div className="text-center space-y-3 pb-8">
-          <p className="text-slate-200 text-8xl">16℃</p>
-          <p className="text-slate-200 text-4xl">Tokyo</p>
+          {weatherData && (
+            <>
+              <p className="text-slate-200 text-8xl">
+                {weatherData.temperature}℃
+              </p>
+              <p className="text-slate-200 text-4xl">{weatherData.location}</p>
+            </>
+          )}
         </div>
-        <div className="flex items-center justify-between gap-4">
-          <WeatherInfo
-            icon={WiHumidity}
-            value="71"
-            label="Humidity"
-            unit={"%"}
-          />
-          <WeatherInfo
-            icon={WiStrongWind}
-            value="3.6"
-            unit={"km/h"}
-            label="Wind Speed"
-          />
-        </div>
+        {weatherData && (
+          <div className="flex items-center justify-between gap-4">
+            <WeatherInfo
+              icon={WiHumidity}
+              value={weatherData.humidity.toString()}
+              label="Humidity"
+              unit="%"
+            />
+            <WeatherInfo
+              icon={WiStrongWind}
+              value={weatherData.windSpeed.toString()}
+              unit="km/h"
+              label="Wind Speed"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
