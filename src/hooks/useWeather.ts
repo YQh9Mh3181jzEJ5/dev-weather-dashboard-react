b765802450532535e23949c32ed1f843782da122
X58okYW8B54e <P4v8AU7pe87R@users.noreply.github.com>
2024-09-05 22:23:15 +0900

    fix: use useQuery insted of normal fetching

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,29 +1,12 @@
-import { useState, useCallback } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { fetchWeatherData } from "../utils/api";
-import { WeatherData } from "../type";
 
-export const useWeather = () => {
-  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const fetchWeather = useCallback(async (city: string) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const data = await fetchWeatherData(city);
-      setWeatherData(data);
-    } catch (error) {
-      if (error instanceof Error) {
-        setError(error.message);
-      } else {
-        setError("An unexpected error occurred");
-      }
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
-
-  return { weatherData, error, isLoading, fetchWeather };
+export const useWeather = (city: string) => {
+  return useQuery({
+    queryKey: ["weather", city],
+    queryFn: () => fetchWeatherData(city),
+    enabled: !!city,
+    staleTime: 5 * 60 * 1000,
+    retry: false,
+  });
 };
