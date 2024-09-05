e23aef5252255fa84026644d313c2944fb26f336
X58okYW8B54e <P4v8AU7pe87R@users.noreply.github.com>
2024-09-05 10:46:00 +0900

    fix: delete some fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: [
-          "Poppins",
-          "ui-sans-serif",
-          "system-ui",
-          "-apple-system",
-          "BlinkMacSystemFont",
-          "Segoe UI",
-          "Roboto",
-          "Helvetica Neue",
-          "Arial",
-          "sans-serif",
-        ],
+        sans: ["Poppins", "sans-serif"],
       },
     },
   },
