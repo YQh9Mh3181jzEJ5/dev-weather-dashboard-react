6d5b8f8f14d76f8dc8391ea0ef6aba8a813e54f8
X58okYW8B54e <P4v8AU7pe87R@users.noreply.github.com>
2024-09-05 12:29:25 +0900

    refactor: add try catch

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,19 +1,28 @@
 import { WeatherData } from "../type";
 
 export const fetchWeatherData = async (city: string): Promise<WeatherData> => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
-    import.meta.env.VITE_APP_ID
-  }`;
-  const response = await fetch(url);
-  const data = await response.json();
-  if (!response.ok) {
-    throw new Error(data.message);
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+      import.meta.env.VITE_APP_ID
+    }`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(
+        data.message || `Failed to fetch weather data for ${city}`
+      );
+    }
+
+    return {
+      icon: data.weather[0].icon,
+      temperature: Math.floor(data.main.temp),
+      humidity: data.main.humidity,
+      windSpeed: data.wind.speed,
+      location: data.name,
+    };
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    throw error;
   }
-  return {
-    icon: data.weather[0].icon,
-    temperature: Math.floor(data.main.temp),
-    humidity: data.main.humidity,
-    windSpeed: data.wind.speed,
-    location: data.name,
-  };
 };
